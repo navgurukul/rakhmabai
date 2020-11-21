9b17084fbac17a11ccfecf3f66ecdabf011497dd
pysaquib <iamsaquibnasim@gmail.com>
2020-11-21 20:52:30 +0530

    ok

--- routes/offerLetter/index.routes.js
+++ routes/offerLetter/index.routes.js
@@ -3,44 +3,26 @@ const multer = require("multer");
 const fs = require("fs");
 const path = require("path");
 const router = express.Router();
-const { main } = require("../../utils/emailSender");
-
-const campus = {
-  Pune: {
-    whatsapp_chat_link: "",
-    facility_in_charge: {
-      name: "",
-      number: "",
-    },
-    location: {},
-  },
-  Bangalore: {
-    whatsapp_chat_link: "",
-    facility_in_charge: {
-      name: "",
-      number: "",
-    },
-    location: {},
-  },
-};
+const { readFile } = require("../../utils/fileHandler");
+const { main } = require("../../utils/offerLetterEmail");
 
 router.post("/offerLetter/sendEmail", async (req, res, next) => {
-  const { name, date, campus } = req.body;
-  await main(
-    allFiles.csv[0],
-    allFiles.attachments,
-    req.body.mailSubject,
-    email,
-    req.body.email,
-    req.body.password
-  );
+  const {
+    receiverEmail,
+    name,
+    date,
+    campus,
+    senderEmail,
+    senderPassword,
+  } = req.body;
+
+  await main(name, date, campus, senderEmail, senderPassword, attachmentFiles);
   // Object.keys(allFiles).forEach((key) => {
   //   allFiles[key].forEach((file) => {
   //     fs.unlinkSync(path.join(__dirname, "../../images/", file));
   //   });
   // });
 
-  allFiles = { attachments: [], csv: [] };
   res.sendStatus(200);
 });
 
--- utils/offerLetterEmail.js
+++ utils/offerLetterEmail.js
@@ -1,14 +1,78 @@
 var nodemailer = require("nodemailer");
 var smtpTransport = require("nodemailer-smtp-transport");
-const parse = require("csv-parse/lib/sync");
 const { readFile } = require("./fileHandler");
 const path = require("path");
+const fs = require("fs");
+
+function getHTML(htmlString, receiverName, campus) {
+  const campusObj = {
+    Pune: {
+      whatsapp_chat_link: "https://chat.whatsapp.com/BWIFHhgIpxXDKDRdNQEv6E",
+      facility_in_charge: {
+        name: "Nitesh",
+        number: "+91-8962158723",
+      },
+      tech_facility_in_charge: {
+        name: "Komal",
+        number: "+91-7302629612",
+      },
+      location: {
+        address:
+          "Flora Institute of Technology, Pune Khed-Shivapur Toll Plaza, Khopi, Maharashtra 412205(Phone: 098906 73701)",
+        link: "https://maps.app.goo.gl/PnrmsVXmEgERXdEf7",
+      },
+    },
+    Bangalore: {
+      whatsapp_chat_link: "https://chat.whatsapp.com/GedtCO1sM2MBfIUGFvB5RU",
+      facility_in_charge: {
+        name: "Me",
+        number: "+91-8971263283",
+      },
+      tech_facility_in_charge: {
+        name: "Shweta",
+        number: "+91-9718602971",
+      },
+      location: { address: "", link: "https://maps.app.goo.gl/aoYyN" },
+    },
+    Dharamshala: {},
+  };
+  htmlString = htmlString.replace(/USERNAME/g, receiverName);
+  htmlString = htmlString.replace(/CAMPUS_OPTION/g, campus);
+  htmlString = htmlString.replace(
+    /WHATSAPP_CHAT_LINK/g,
+    campusObj[campus].whatsapp_chat_link
+  );
+  htmlString = htmlString.replace(
+    /CAMPUS_FACILITY_INCHARGE/g,
+    campusObj[campus].facility_in_charge.name
+  );
+  htmlString = htmlString.replace(
+    /CAMPUS_FACILITY_INCHARGE_NUMBER/g,
+    campusObj[campus].facility_in_charge.number
+  );
+  htmlString = htmlString.replace(
+    /TECH_FACILITY_INCHARGE/g,
+    campusObj[campus].tech_facility_in_charge.name
+  );
+  htmlString = htmlString.replace(
+    /TECH_FACILITY_INCHARGE_NUMBER/g,
+    campusObj[campus].tech_facility_in_charge.number
+  );
+  htmlString = htmlString.replace(
+    /CAMPUS_ADDRESS/g,
+    campusObj[campus].location.address
+  );
+  htmlString = htmlString.replace(
+    /MAPS_LINK/g,
+    campusObj[campus].location.link
+  );
+  return htmlString;
+}
 
 async function main(
-  csvFile,
-  attachmentsFile,
-  htmlSubject,
-  htmlBody,
+  receiverEmail,
+  receiverName,
+  campus,
   senderEmail,
   senderPassword
 ) {
@@ -31,51 +95,31 @@ async function main(
     attachments: [],
   };
 
-  function getHTML(htmlString, name) {
-    if (name.length > 0) name = " " + name;
-    return htmlString.replace("username", name);
-  }
+  var htmlString = await readFile(__dirname + "/content.html");
 
-  var rows = [];
+  const attachmentsDir = path.join(
+    __dirname,
+    `../images/offerLetter/${campus}`
+  );
 
-  async function sendEmails() {
-    var fileData = await readFile(path.join(__dirname, "../images", csvFile));
-    rows = parse(fileData, {
-      columns: false,
-      trim: true,
-      skip_empty_lines: true,
+  const attachmentFiles = fs.readdirSync(attachmentsDir);
+  attachmentFiles.forEach((file) => {
+    mailOptions.attachments.push({
+      fileName: file,
+      path: path.join(),
     });
-    send_email(rows, 1, htmlBody);
-  }
+  });
 
-  sendEmails();
+  mailOptions.html = getHTML(htmlString, receiverName, campus);
+  mailOptions.to = receiverEmail + "<" + receiverEmail + ">";
 
-  async function send_email(rows, i, htmlString) {
-    attachmentsFile.forEach((file) => {
-      let eachPath = path.join(__dirname, "../images/", file);
-      mailOptions.attachments.push({
-        filename: file,
-        path: eachPath,
-      });
-    });
-    mailOptions.html = getHTML(htmlString, rows[i][0]);
-    mailOptions.to = rows[i][0] + "<" + rows[i][1] + ">";
-
-    await transporter.sendMail(mailOptions, function (err, info) {
-      if (err) console.log(err);
-      else {
-        console.log(info);
-        console.log("Sent", i + 1, "emails. Next index: ", i + 1);
-
-        if (i < rows.length - 1) {
-          setTimeout(
-            () => send_email(rows, i + 1, htmlString),
-            Math.random() * 1000
-          );
-        }
-      }
-    });
-  }
+  await transporter.sendMail(mailOptions, function (err, info) {
+    if (err) console.log(err);
+    else {
+      console.log(info);
+      console.log("Sent", i + 1, "emails. Next index: ", i + 1);
+    }
+  });
 }
 
 module.exports = { main };
