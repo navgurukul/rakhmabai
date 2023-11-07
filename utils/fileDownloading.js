4c5592702a7c4227c0cc183e24a94109ade094c0
ujjwal15718 <ujjwal21@navgurukul.org>
2023-11-07 21:18:45 +0530

    modify the downloading process that saves time and remove to download the pdf files

--- utils/fileDownloading.js
+++ utils/fileDownloading.js
@@ -4,61 +4,55 @@ const path = require('path');
 
 async function downloadFiles(response) {
     try {
-        // const response = await axios.get('https://merd-strapi.merakilearn.org/api/offer-letters?populate=cc,attachment.files');
-
         let offerLetters = response;
-        // offerLetters = offerLetters.filter((item) => item.attributes.campusName == 'Dantewada');
-        // console.log(offerLetters, '>>>>>>>>>>>>')
-
-        // Define the destination folder
-        const pdfFolder = path.join(__dirname, 'pdfeditor');
+        // Define the destination folder for docx files
         const docxFolder = path.join(__dirname, 'docxeditor');
 
-        // Clear the destination folders
-        clearFolder(pdfFolder);
+        // Clear the destination docx folder
         clearFolder(docxFolder);
+
+        const downloadPromises = [];
 
         for (const offerLetter of offerLetters) {
             const attachments = offerLetter.attributes.attachment;
-            // console.log(attachments,'>>>>>>>>>>>>')
             for (const key in attachments) {
                 if (attachments.hasOwnProperty(key)) {
                     const attachment = attachments[key];
-                    // console.log(attachment, '>>>>>>>>>>>>');
                     const fileUrl = attachment.files.data.attributes.url;
-                    // console.log(fileUrl, '>>>>>>>');
                     const fileName = attachment.files.data.attributes.name;
                     const fileExtension = path.extname(fileName);
-                    const destinationFolder = fileExtension === '.pdf' ? pdfFolder : docxFolder;
 
-                    // Create the destination folder if it doesn't exist
-                    if (!fs.existsSync(destinationFolder)) {
-                        fs.mkdirSync(destinationFolder, { recursive: true });
+                    // Check if the file extension is .docx
+                    if (fileExtension.toLowerCase() === '.docx') {
+                        const filePath = path.join(docxFolder, fileName);
+                        const downloadPromise = downloadFile(fileUrl, filePath);
+                        downloadPromises.push(downloadPromise);
                     }
-
-                    const filePath = path.join(destinationFolder, fileName);
-
-                    const fileResponse = await axios({
-                        url: fileUrl,
-                        method: 'GET',
-                        responseType: 'stream',
-                    });
-
-                    const writer = fs.createWriteStream(filePath);
-                    fileResponse.data.pipe(writer);
-
-                    await new Promise((resolve, reject) => {
-                        writer.on('finish', resolve);
-                        writer.on('error', reject);
-                    });
-
-                    console.log(`File downloaded: ${filePath}`);
                 }
             }
         }
+
+        await Promise.all(downloadPromises);
+        console.log('All files downloaded successfully.');
     } catch (error) {
         console.error('Error downloading files:', error);
     }
+}
+
+async function downloadFile(url, filePath) {
+    const response = await axios({
+        url: url,
+        method: 'GET',
+        responseType: 'stream',
+    });
+
+    const writer = fs.createWriteStream(filePath);
+
+    return new Promise((resolve, reject) => {
+        response.data.pipe(writer);
+        writer.on('finish', resolve);
+        writer.on('error', reject);
+    });
 }
 
 function clearFolder(folderPath) {
